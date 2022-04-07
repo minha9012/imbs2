41f1b00619ee8018f3628f7381b58a72d09e71d9
minha <minha1002@gmail.com>
2022-04-07 20:51:26 +0900

    by minggu

---
+++ src/main/java/com/example/tonacup/TonacupApplication.java
@@ -0,0 +1,13 @@
+package com.example.tonacup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonacupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonacupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tonacup/TonacupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tonacup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonacupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
